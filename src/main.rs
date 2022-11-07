3950624663bd8d248c5b861fd8625a5035b436be
Amit Aryeh Levy <amit@amitlevy.com>
2022-11-07 16:05:10 -0500

    Basic protocol support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,10 @@
+use std::sync::Arc;
 use std::sync::mpsc::sync_channel;
 use std::thread;
 use std::{path::Path, io::stdin};
 use clap::{Parser, Subcommand};
+use serde::{Serialize, Deserialize};
+use serde_json::Value;
 
 mod frida;
 mod storage;
@@ -29,26 +32,78 @@ struct Cli {
     command: Commands,
 }
 
+#[derive(Debug, Deserialize, Serialize)]
+struct Group {
+    admins: Vec<String>,
+    parents: Vec<String>,
+    children: Option<Vec<String>>,
+    writers: Vec<String>,
+    #[serde(rename = "contactLevel")]
+    contact_level: bool,
+    name: Option<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(untagged)]
+enum GroupOrId {
+    Group {
+        id: String,
+        value: Group,
+    },
+    Id(String),
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+#[serde(tag = "msgType")]
+enum KahloMessage {
+    /// TODO: Probably doesn't belong at this level of the protocol
+    #[serde(rename = "requestUpdateLinked")]
+    RequestUpdateLinked {
+        #[serde(rename = "newLinkedMembers")]
+        new_linked_members: Vec<GroupOrId>,
+        #[serde(rename = "tempName")]
+        temp_name: String,
+        #[serde(rename = "srcIdkey")]
+        src_id_key: String,
+    },
+    #[serde(rename = "updateData")]
+    UpdateData {
+        key: String,
+        value: Value,
+    },
+    #[serde(rename = "deleteData")]
+    DeleteData {
+        key: String,
+    },
+}
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let cli = Cli::parse();
 
-    let frida = frida::Frida::new(storage::DB::new(Path::new(&cli.frida_storage))?)?;
+    let frida = Arc::new(frida::Frida::new(storage::DB::new(Path::new(&cli.frida_storage))?)?);
 
     let app_db = storage::DB::new(Path::new(&cli.app_storage))?;
 
     let (sender, receiver) = sync_channel(10);
     frida.connect_socketio(sender)?;
-    let device_id = frida.device_id.clone();
-    thread::spawn(move || {
-        for (s, msg) in receiver.iter() {
-            if s == device_id {
-                println!("> self {}", String::from_utf8_lossy(msg.as_slice()));
-            } else {
-                println!("> {} {}", s, String::from_utf8_lossy(msg.as_slice()));
+
+    {
+        let frida = frida.clone();
+        thread::spawn(move || {
+            for (s, msg) in receiver.iter() {
+                if s == frida.device_id {
+                    println!("> self {}", String::from_utf8_lossy(msg.as_slice()));
+                } else {
+                    if let Ok(kahlo_msg) = serde_json::from_slice::<KahloMessage>(msg.as_slice()) {
+                        println!("> {} {:?}", s, kahlo_msg);
+                    } else {
+                        println!("> {} {}", s, String::from_utf8_lossy(msg.as_slice()));
+                    }
+                }
             }
-        }
-    });
+        });
+    }
 
     match cli.command {
         Commands::Init => {
